4f6ee14ecbabe6c071cc8191515dcbda28841244
Mark Strother <mark@pacific.ca>
2016-12-22 10:12:38 -0800

    Add BmpListener

---
+++ BMPClient/BmpListener.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using BmpListener.BMP;
+
+namespace BmpListener
+{
+    public class BmpListener
+    {
+        private readonly TcpListener tcpListener;
+
+        public BmpListener(IPAddress ip, int port = 11019)
+        {
+            tcpListener = new TcpListener(ip, port);
+        }
+
+        public BmpListener(int port = 11019)
+        {
+            tcpListener = new TcpListener(IPAddress.IPv6Any, port);
+            tcpListener.Server.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+        }
+
+        private async Task AcceptTcpClient(CancellationToken cts)
+        {
+            while (!cts.IsCancellationRequested)
+            {
+                var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                var task = ProcessClientAsync(tcpClient);
+            }
+        }
+
+        private static async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            while (true)
+                using (var stream = tcpClient.GetStream())
+                {
+                    var bmpHeaderBytes = new byte[6];
+                    var bmpPeerHeaderBytes = new byte[42];
+                    await stream.ReadAsync(bmpHeaderBytes, 0, 6); //add cancellation token
+                    var header = new Header(bmpHeaderBytes);
+                    var message = new BMPMessage(header);
+                    if (header.Type != BMPMessage.BMPMessageType.Initiation)
+                    {
+                        await stream.ReadAsync(bmpPeerHeaderBytes, 0, 42); //add cancellation token
+                        message.PeerHeader = new PeerHeader(bmpPeerHeaderBytes);
+                        var bmpMsgBytes = new byte[header.Length - 48];
+                        await stream.ReadAsync(bmpMsgBytes, 0, bmpMsgBytes.Length);
+                        switch (header.Type)
+                        {
+                            case BMPMessage.BMPMessageType.RouteMonitoring:
+                                message.Body = new RouteMonitoring(message, bmpMsgBytes);
+                                break;
+                            case BMPMessage.BMPMessageType.StatisticsReport:
+                                message.Body = new StatisticsReport();
+                                break;
+                            case BMPMessage.BMPMessageType.PeerDown:
+                                break;
+                            case BMPMessage.BMPMessageType.PeerUp:
+                                message.Body = new PeerUpNotification(message, bmpMsgBytes);
+                                break;
+                            case BMPMessage.BMPMessageType.Initiation:
+                                message.Body = new BMPInitiation();
+                                break;
+                            case BMPMessage.BMPMessageType.Termination:
+                                message.Body = new BMPTermination();
+                                break;
+                        }
+                        //WriteJson(message);
+                    }
+                }
+        }
+    }
+}
