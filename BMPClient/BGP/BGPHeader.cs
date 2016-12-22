38f3fc43b4157c5541fed4b7d09b812cf5dfec56
Mark Strother <mark@pacific.ca>
2016-12-22 09:44:32 -0800

    Remove marker

--- BMPClient/BGP/BGPHeader.cs
+++ BMPClient/BGP/BGPHeader.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using Newtonsoft.Json;
 
 namespace BmpListener.BGP
 {
@@ -11,10 +10,7 @@ namespace BmpListener.BGP
             Length = data.ToUInt16(16);
             Type = (BGP.MessageType)data.ElementAt(18);
         }
-
-        [JsonIgnore]
-        public byte[] Marker { get; }
-
+        
         public uint Length { get; }
         public BGP.MessageType Type { get; }
     }
