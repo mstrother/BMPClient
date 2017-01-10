f9c1e9ba1338ac7167b622f8d9748bef52efba93
Mark Strother <mark@pacific.ca>
2017-01-10 01:22:45 -0800

    Remove length properties

--- BmpListener/Bgp/BgpUpdateMessage.cs
+++ BmpListener/Bgp/BgpUpdateMessage.cs
@@ -1,38 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace BmpListener.Bgp
 {
     public sealed class BgpUpdateMessage : BgpMessage
     {
+        private readonly List<PathAttribute> pathAttributes = new List<PathAttribute>();
+        private readonly List<IPAddrPrefix> withDrawnRoutes = new List<IPAddrPrefix>();
+
         public BgpUpdateMessage(ArraySegment<byte> data) : base(ref data)
         {
             DecodeFromBytes(data);
         }
 
-        private readonly List<PathAttribute> pathAttributes = new List<PathAttribute>();
-        private readonly List<IPAddrPrefix> withDrawnRoutes = new List<IPAddrPrefix>();
-
-        [JsonIgnore]
-        public ushort WithdrawnRoutesLength { get; private set; }
-
         public IPAddrPrefix[] WithdrawnRoutes => withDrawnRoutes.ToArray();
-
-        [JsonIgnore]
-        public ushort TotalPathAttributeLength { get; private set; }
-
         public PathAttribute[] PathAttributes => pathAttributes.ToArray();
         public IPAddrPrefix[] NLRI { get; private set; }
 
 
         public override void DecodeFromBytes(ArraySegment<byte> data)
         {
-            WithdrawnRoutesLength = data.ToUInt16(0);
-            TotalPathAttributeLength = data.ToUInt16(2);
+            var withdrawnRoutesLength = data.ToUInt16(0);
+            var totalPathAttributeLength = data.ToUInt16(2);
             var offset = 23;
 
-            for (int i = WithdrawnRoutesLength; i < WithdrawnRoutesLength;)
+            for (int i = withdrawnRoutesLength; i < withdrawnRoutesLength;)
             {
                 //var prefix = new byte[5];
                 //Buffer.BlockCopy(data.ToArray(), offset, prefix, 0, 5);
@@ -44,24 +36,24 @@ namespace BmpListener.Bgp
                 //offset += 5;
             }
 
-            for (int i = TotalPathAttributeLength; i > 0;)
+            for (int i = totalPathAttributeLength; i > 0;)
             {
                 var attrBytes = new ArraySegment<byte>(data.Array, offset, i);
                 var pathAttribute = PathAttribute.GetPathAttribute(attrBytes);
                 if (pathAttribute.Flags.HasFlag(AttributeFlags.ExtendedLength))
                 {
-                    offset += (int)pathAttribute.Length + 4;
-                    i -= (int)pathAttribute.Length + 4;
+                    offset += (int) pathAttribute.Length + 4;
+                    i -= (int) pathAttribute.Length + 4;
                 }
                 else
                 {
-                    offset += (int)pathAttribute.Length + 3;
-                    i -= (int)pathAttribute.Length + 3;
+                    offset += (int) pathAttribute.Length + 3;
+                    i -= (int) pathAttribute.Length + 3;
                 }
                 pathAttributes.Add(pathAttribute);
             }
 
-            var nlriLength = data.Array.Length - 23 - TotalPathAttributeLength - WithdrawnRoutesLength;
+            var nlriLength = data.Array.Length - 23 - totalPathAttributeLength - withdrawnRoutesLength;
         }
     }
 }
