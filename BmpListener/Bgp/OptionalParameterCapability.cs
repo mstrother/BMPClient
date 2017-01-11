b1c7e996c936c1a22ebcfd95ed2cfffb0f359c46
Mark Strother <mark@pacific.ca>
2017-01-11 13:55:42 -0800

    Rename parameter

--- BmpListener/Bgp/OptionalParameterCapability.cs
+++ BmpListener/Bgp/OptionalParameterCapability.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BmpListener.Bgp
 {
     public class OptionalParameterCapability : OptionalParameter
     {
-        public OptionalParameterCapability(ArraySegment<byte> data) : base(data)
+        public OptionalParameterCapability(ArraySegment<byte> data) : base(ref data)
         {
             Decode(data);
         }
 
-        public Capability[] ParameterValue { get; private set; }
+        public Capability[] Capabilities { get; private set; }
 
         public void Decode(ArraySegment<byte> data)
         {
             var capabilities = new List<Capability>();
-            var bytes = new byte[ParameterLength];
-            Array.Copy(data.Array, data.Offset + 2, bytes, 0, ParameterLength);
+
+            //var offset = data.Offset + 10;
+            //var length = (int)data.ElementAt(9);
+            //data = new ArraySegment<byte>(data.Array, offset, length);
+            //while (data.Count > 0)
+            //{
+            //    //TODO if OptParamLength <2 return an error  
+            //    var optParam = OptionalParameter.GetOptionalParameter(data);
+            //    OptionalParameters.Add(optParam);
+            //    offset = data.Offset + optParam.ParameterLength + 2;
+            //    length = data.Count - optParam.ParameterLength - 2;
+            //    data = new ArraySegment<byte>(data.Array, offset, length);
+            //}
 
             for (var i = 0; i < ParameterLength;)
             {
-                var type = (Capability.CapabilityCode) bytes[i];
+                var type = (Capability.CapabilityCode) data.First();
                 i++;
-                var length = bytes[i];
+                var length = data.ElementAt(i);
                 i++;
                 data = new ArraySegment<byte>(bytes, i, length);
                 var capability = Capability.GetCapability(type, data);
@@ -30,7 +42,7 @@ namespace BmpListener.Bgp
                     capabilities.Add(capability);
                 i += length;
             }
-            ParameterValue = capabilities.ToArray();
+            Capabilities = capabilities.ToArray();
         }
     }
 }
