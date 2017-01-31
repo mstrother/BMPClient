e66c966c0a163ea7010b0d7a7af46ce56ede0957
Mark Strother <mark@pacific.ca>
2017-01-31 02:33:18 -0800

    Add large BGP communities support

--- BmpListener/Bgp/PathAttributeLargeCommunities.cs
+++ BmpListener/Bgp/PathAttributeLargeCommunities.cs
@@ -4,8 +4,25 @@ namespace BmpListener.Bgp
 {
     internal class PathAttributeLargeCommunities : PathAttribute
     {
+        private int asn;
+        private int data1;
+        private int data2;
+
         public PathAttributeLargeCommunities(ArraySegment<byte> data) : base(ref data)
         {
+            DecodeFromByes(data);
+        }
+
+        public void DecodeFromByes(ArraySegment<byte> data)
+        {
+            asn = data.ToInt32(0);
+            data1 = data.ToInt32(4);
+            data2 = data.ToInt32(8);
+        }
+
+        public override string ToString()
+        {
+            return ($"{asn}:{data1}:{data2}");
         }
     }
 }
