c1eb5b2fc85d6f54e7b523fd3efc8df47b26663e
Mark Strother <mark@pacific.ca>
2017-03-03 07:33:14 -0800

    Refactor to improve readability

--- BmpListener/Bmp/BmpMessage.cs
+++ BmpListener/Bmp/BmpMessage.cs
@@ -4,14 +4,19 @@ namespace BmpListener.Bmp
 {
     public abstract class BmpMessage
     {
-        public BmpMessage(BmpHeader header, ref ArraySegment<byte> data)
+        // RFC 7854
+        private const int PerPeerHeaderLength = 42;
+
+        protected BmpMessage(BmpHeader header, ArraySegment<byte> data)
         {
             BmpHeader = header;
-            PeerHeader = new BmpPeerHeader(data);
-            data = new ArraySegment<byte>(data.Array, 42, data.Array.Length - 42);
+            PeerHeader = new PerPeerHeader(data);
+            var offset = PerPeerHeaderLength;
+            var count = data.Array.Length - offset;
+            MessageData = new ArraySegment<byte>(data.Array, offset, count);
         }
 
-        public BmpMessage(BmpHeader header)
+        protected BmpMessage(BmpHeader header)
         {
             BmpHeader = header;
         }
@@ -27,27 +32,28 @@ namespace BmpListener.Bmp
             RouteMirroring
         }
 
+        protected ArraySegment<byte> MessageData { get; }
         public BmpHeader BmpHeader { get; private set; }
-        public BmpPeerHeader PeerHeader { get; set; }
-            
-        public static BmpMessage GetBmpMessage(BmpHeader bmpHeader)
+        public PerPeerHeader PeerHeader { get; private set; }
+
+        public static BmpMessage Create(BmpHeader bmpHeader)
         {
-            var data = new ArraySegment<byte>();
-            return Create(bmpHeader, data);
+            return new BmpInitiation(bmpHeader);
         }
 
-        public static BmpMessage Create(BmpHeader bmpHeader, ArraySegment<byte> data)
+        public static BmpMessage Create(BmpHeader bmpHeader, byte[] data)
         {
+            var msg = new ArraySegment<byte>(data);
             switch (bmpHeader.MessageType)
             {
                 case Type.RouteMonitoring:
-                    return new RouteMonitoring(bmpHeader, data);
+                    return new RouteMonitoring(bmpHeader, msg);
                 case Type.StatisticsReport:
-                    return new StatisticsReport(bmpHeader, data);
+                    return new StatisticsReport(bmpHeader, msg);
                 case Type.PeerDown:
-                    return new PeerDownNotification(bmpHeader, data);
+                    return new PeerDownNotification(bmpHeader, msg);
                 case Type.PeerUp:
-                    return new PeerUpNotification(bmpHeader, data);
+                    return new PeerUpNotification(bmpHeader, msg);
                 case Type.Initiation:
                     return new BmpInitiation(bmpHeader);
                 case Type.Termination:
