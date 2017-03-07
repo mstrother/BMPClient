940409bb96247ca700739fa6999b0cc15fbc37e1
Mark Strother <mark@pacific.ca>
2017-03-07 04:29:08 -0800

    Add BgpHeaderLength

--- BmpListener/Constants.cs
+++ BmpListener/Constants.cs
@@ -2,6 +2,7 @@
 {
     static class Constants
     {
+        public const int BgpHeaderLength = 19;
         public const int BmpCommonHeaderLength = 6;
         public const int BmpPerPeerHeaderLength = 42;
     }
