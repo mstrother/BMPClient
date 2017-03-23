1abb00ca4e60b3123526243f2ffe52d51a1dd36b
Mark Strother <mark@pacific.ca>
2017-03-23 06:00:02 -0700

    Remove cast

--- src/BmpListener/Bmp/RouteMonitoring.cs
+++ src/BmpListener/Bmp/RouteMonitoring.cs
@@ -15,7 +15,7 @@ namespace BmpListener.Bmp
 
         public void Decode(byte[] data, int offset)
         {
-            BgpMessage = (BgpUpdateMessage)BgpMessage.Create(data, offset);
+            BgpMessage = BgpMessage.Create(data, offset);
         }
     }
 }
