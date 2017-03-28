1ff840ccc625bee981b465e85ff8430a4f54c3be
Mark Strother <mark@pacific.ca>
2017-03-27 18:05:04 -0700

    Restructure folder layout

---
+++ src/BmpListener/BmpMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using BmpListener.Bmp;
+using DotNetty.Transport.Channels;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace BmpListener
+{
+    public class BmpMessageHandler : SimpleChannelInboundHandler<BmpMessage>
+    {
+        Subject<BmpMessage> onMessageReceived = new Subject<BmpMessage>();
+
+        public IObservable<BmpMessage> OnMessageReceived
+        {
+            get { return onMessageReceived.AsObservable(); }
+        }
+        
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception e) => context.CloseAsync();
+
+        protected override void ChannelRead0(IChannelHandlerContext context, BmpMessage msg)
+        {
+            try
+            {
+                onMessageReceived.OnNext(msg);
+            }
+            catch (Exception ex)
+            {
+                onMessageReceived.OnError(ex);
+            }
+        }
+    }
+}
