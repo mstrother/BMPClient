2fcc8e44255eb7693493fe9bd7872f3793fe0141
Mark Strother <mark@pacific.ca>
2017-04-05 03:47:12 -0700

    Add Decode method

--- src/BmpListener/Bgp/OptionalParameter.cs
+++ src/BmpListener/Bgp/OptionalParameter.cs
@@ -1,15 +1,18 @@
 ﻿
+using System;
+using System.Linq;
+
 namespace BmpListener.Bgp
 {
     public abstract class OptionalParameter
     {
-        public OptionalParameter(byte[] data, int offset)
-        {
-            ParameterType = data[offset];
-            ParameterLength = data[offset + 1];
-        }
+        public OptionalParameterType Type { get; private set; }
+        public int Length { get; private set; }
 
-        public int ParameterType { get; }
-        public int ParameterLength { get; }
+        public virtual void Decode(ArraySegment<byte> data)
+        {
+            Type = (OptionalParameterType)data.First();
+            Length = data.ElementAt(1);
+        }
     }
 }
