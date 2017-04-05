208e37163a9a9beaed35af46fc3a77eb7cf8bb2b
Mark Strother <mark@pacific.ca>
2017-04-05 03:46:38 -0700

    Add class

---
+++ src/BmpListener/Bgp/CapabilitiesParameter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BmpListener.Bgp
+{
+    public class CapabilitiesParameter : OptionalParameter
+    {
+        public IList<Capability> Capabilities { get; } = new List<Capability>();
+
+        public override void Decode(ArraySegment<byte> data)
+        {
+            base.Decode(data);
+            data = new ArraySegment<byte>(data.Array, data.Offset + 2, Length);
+
+            for (var i = 0; i < Length;)
+            {
+                (Capability capability, int length) = Capability.DecodeCapability(data);
+                Capabilities.Add(capability);
+                data = new ArraySegment<byte>(data.Array, data.Offset + length, data.Count - length);
+                i += length;
+            }
+        }
+    }
+}
