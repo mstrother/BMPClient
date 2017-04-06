2f98b7eb556c54dd413210e4ee70ce76562ce260
Mark Strother <mark@pacific.ca>
2017-04-06 02:06:01 -0700

    Add notification message support

--- src/BmpListener/Bgp/BgpNotificationMessage.cs
+++ src/BmpListener/Bgp/BgpNotificationMessage.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 namespace BmpListener.Bgp
 {
     public sealed class BgpNotificationMessage : BgpMessage
     {
+        public NotificationErrorCode ErrorCode { get; private set; }
+        public int ErrorSubCode { get; private set; }
+
         public override void Decode(ArraySegment<byte> data)
         {
-            throw new NotImplementedException();
+            ErrorCode = (NotificationErrorCode)data.First();
+            ErrorSubCode = data.ElementAt(1);
         }
     }
 }
---
+++ src/BmpListener/Bgp/NotificationErrorCode.cs
@@ -0,0 +1,14 @@
+﻿namespace BmpListener.Bgp
+{
+    // RFC 4271 4.5
+    public enum NotificationErrorCode
+    {
+        MessageHeaderError = 1,
+        OPENMessageError,
+        UPDATEMessageError,
+        HoldTimerExpired,
+        FiniteStateMachineError,
+        Cease,
+        RouteRefreshMessageError
+    }
+}
