0fec24c844b9b0fed31569b03ee12fe89e3629e0
Mark Strother <mark@pacific.ca>
2017-04-16 04:49:20 -0700

    Add BGP and BMP message lengths to JSON

--- src/BmpListener/Serialization/Models/UpdateModel.cs
+++ src/BmpListener/Serialization/Models/UpdateModel.cs
@@ -4,6 +4,8 @@ namespace BmpListener.Serialization.Models
 {
     public class UpdateModel
     {
+        public int BmpMsgLength { get; set; }
+        public int BgpMsgLength { get; set; }
         public PeerHeaderModel Peer { get; set; }
         public string Origin { get; set; }
         public IList<int> AsPath { get; set; }
