bb716142f768815dc149a0441f80ba73c4f33857
Mark Strother <mark@pacific.ca>
2017-04-20 04:52:32 -0700

    Add models

---
+++ src/BmpListener/Serialization/Models/PrefixAnnounceModel.cs
@@ -0,0 +1,16 @@
+﻿using BmpListener.Bgp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace BmpListener.Serialization.Models
+{
+    public class PrefixAnnounceModel
+    {
+        public AddressFamily Afi { get; set; }
+        public SubsequentAddressFamily Safi { get; set; }
+        public IPAddress Nexthop { get; set; }
+        public IList<IPAddrPrefix> Prefixes { get; set; } = new List<IPAddrPrefix>();
+    }
+}
---
+++ src/BmpListener/Serialization/Models/PrefixWithdrawModel.cs
@@ -0,0 +1,14 @@
+﻿using BmpListener.Bgp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BmpListener.Serialization.Models
+{
+    public class PrefixWithdrawal
+    {
+        public AddressFamily Afi { get; set; }
+        public SubsequentAddressFamily Safi { get; set; }
+        public IList<IPAddrPrefix> Prefixes { get; set; } = new List<IPAddrPrefix>();
+    }
+}
