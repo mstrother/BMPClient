d46d27e91f876b706e1fcbfb5061e186df5e9f25
Mark Strother <mark@pacific.ca>
2017-04-22 02:19:21 -0700

    Add converter

---
+++ src/BmpListener/Serialization/Converters/PathAttributeOriginConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BmpListener.Bgp;
+using Newtonsoft.Json;
+
+namespace BmpListener.Serialization.Converters
+{
+    public class PathAttributeOriginConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(PathAttributeOrigin);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            var origin = (PathAttributeOrigin)value;
+
+            switch (origin.Origin)
+            {
+                case PathAttributeOrigin.Type.IGP:
+                    writer.WriteValue("igp");
+                    break;
+                case PathAttributeOrigin.Type.EGP:
+                    writer.WriteValue("egp");
+                    break;
+                case PathAttributeOrigin.Type.Incomplete:
+                    writer.WriteValue("incomplete");
+                    break;
+                default:
+                    writer.WriteValue("incomplete");
+                    break;
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
