2e48549f51f7102eea681d28dd40db10802f28ba
Mark Strother <mark@pacific.ca>
2017-04-22 03:34:31 -0700

    Add classes

---
+++ src/BmpListener/Bmp/BmpInitiationMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace BmpListener.Bmp
+{
+    public class BmpInitiation : BmpMessage
+    {
+    }
+}
---
+++ src/BmpListener/Bmp/TerminationMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BmpListener.Bmp
+{
+    public class BmpTermination : BmpMessage
+    {
+        public override void Decode(byte[] data, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
