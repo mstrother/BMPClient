02b5ebf1a741981e23248ee255ba9f5898de4524
Mark Strother <mark@pacific.ca>
2017-04-22 03:37:00 -0700

    Encapsulate BGP update messages only

--- src/BmpListener/Bmp/RouteMonitoring.cs
+++ src/BmpListener/Bmp/RouteMonitoring.cs
@@ -1,15 +1,14 @@
 ﻿using BmpListener.Bgp;
-using System;
 
 namespace BmpListener.Bmp
 {
     public class RouteMonitoring : BmpMessage
     {
-        public BgpMessage BgpMessage { get; set; }
+        public BgpUpdateMessage BgpUpdate { get; set; }
 
         public override void Decode(byte[] data, int offset)
         {
-            BgpMessage = BgpMessage.DecodeMessage(data, offset);
+            BgpUpdate = BgpMessage.DecodeMessage(data, offset) as BgpUpdateMessage;
         }
     }
 }
