0cf07b7395fbbd0e6f14956a7e7bbd8c8d42ab81
Mark Strother <mark@pacific.ca>
2017-04-26 13:05:49 -0700

    New console example

--- examples/BmpListener.ConsoleExample/Program.cs
+++ examples/BmpListener.ConsoleExample/Program.cs
@@ -1,14 +1,19 @@
-﻿using BmpListener.Bmp;
-using System;
-using System.Reactive.Linq;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using BmpListener.Bmp;
+using BmpListener.Serialization;
 
 namespace BmpListener.ConsoleExample
 {
     internal class Program
     {
-        IDisposable bmpSubscription;
+        static readonly IDisposable BmpMessageSubscription;
+        
+        static Program()
+        {
+            BmpMessageSubscription = BmpMessageHandler.MessageReceived.Subscribe(WriteJson);
+        }
 
         private static void Main()
         {
@@ -17,23 +22,25 @@ namespace BmpListener.ConsoleExample
             //ThreadPool.SetMinThreads(minWorkerThreads, Math.Max(16, minCompletionPortThreads));
 
             int threadCount = Environment.ProcessorCount;
-
+            
             var cts = new CancellationTokenSource();
             var bmpListener = new BmpListener(11019);
 
             Console.WriteLine($"Starting new BMP agent on port {bmpListener.Port}.");
             Task.Run(() => bmpListener.StartAsync(cts.Token), cts.Token);
             Console.WriteLine($"BMP agent started at {DateTime.UtcNow:hh\\:mm\\:ss} (UTC).");
-            Console.WriteLine();
+            //Console.WriteLine();
             Console.WriteLine("Press any key to shutdown.");
-
-            var logger = new ConsoleLogger();
-            //Task.Run(() => logger.StartAsync(cts.Token), cts.Token);
-            //bmpListener.OnMessageReceived.Subscribe(logger.LogMessageAsync);
-
+            
             Console.ReadKey(true);
             cts.Cancel();
-            bmpListener.CloseCompletion.Wait(TimeSpan.FromSeconds(20));
-        }        
+            //bmpListener.CloseCompletion.Wait(TimeSpan.FromSeconds(20));
+        }
+
+        static void WriteJson(BmpMessage msg)
+        {
+            var json = msg.ToJson();
+            Console.WriteLine(json);
+        }
     }
 }
