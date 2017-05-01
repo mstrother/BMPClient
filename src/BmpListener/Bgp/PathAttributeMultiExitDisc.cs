274f1b7b065a4ca149a95174605e5e6d825de560
Mark Strother <mark@pacific.ca>
2017-05-01 15:24:14 -0700

    Add multi-exit discriminator (MED) support

--- src/BmpListener/Bgp/PathAttributeMultiExitDisc.cs
+++ src/BmpListener/Bgp/PathAttributeMultiExitDisc.cs
@@ -1,13 +1,14 @@
-﻿namespace BmpListener.Bgp
+﻿using BmpListener.Utilities;
+
+namespace BmpListener.Bgp
 {
     internal class PathAttributeMultiExitDisc : PathAttribute
     {
-        public int? Metric { get; private set; }
+        public uint Metric { get; private set; }
 
         public override void Decode(byte[] data, int offset)
         {
-            //Array.Reverse(data, offset, 0);
-            //Metric = BitConverter.ToInt32(data, 0);
+            Metric = EndianBitConverter.Big.ToUInt32(data, offset);
         }
     }
 }
